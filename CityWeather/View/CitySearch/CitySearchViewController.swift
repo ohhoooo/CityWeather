1842d15ad473746dee241108a9bb0378ea9fb0ee
KJH <gh7052@gmail.com>
2025-02-16 20:44:15 +0900

    feat: CitySearchViewController Custom Observable Class 기반 MVVM+Input/Output 구조로 데이터 바인딩 기능 구현

--- CityWeather/View/CitySearch/CitySearchViewController.swift
+++ CityWeather/View/CitySearch/CitySearchViewController.swift
@@ -11,7 +11,7 @@ final class CitySearchViewController: BaseViewController {
     
     // MARK: - properties
     private let citySearchView = CitySearchView()
-    let viewModel = CitySearchViewModel()
+    private let viewModel = CitySearchViewModel()
     
     // MARK: - life cycles
     override func loadView() {
@@ -21,24 +21,34 @@ final class CitySearchViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        viewModel.input.didLoadView.value = ()
     }
     
     // MARK: - methods
     override func configureStyle() {
         configureNavigation()
+        citySearchView.tableView.separatorStyle = .none
     }
     
     override func configureDelegate() {
-        
-    }
-    
-    override func configureAddTarget() {
-        
+        citySearchView.searchBar.delegate = self
+        citySearchView.tableView.delegate = self
+        citySearchView.tableView.dataSource = self
+        citySearchView.tableView.register(CitySearchCell.self, forCellReuseIdentifier: CitySearchCell.identifier)
     }
     
     override func bind() {
+        viewModel.output.weathers.lazyBind { [weak self] weathers in
+            self?.citySearchView.hasData(hasData: !weathers.isEmpty)
+        }
         
+        viewModel.output.iconImages.lazyBind { [weak self] _ in
+            self?.citySearchView.tableView.reloadData()
+        }
+        
+        viewModel.output.cityView.lazyBind { [weak self] _ in
+            self?.navigationController?.popViewController(animated: true)
+        }
     }
     
     private func configureNavigation() {
@@ -46,5 +56,49 @@ final class CitySearchViewController: BaseViewController {
         navigationItem.largeTitleDisplayMode = .never
         navigationController?.navigationBar.tintColor = .black
         navigationController?.navigationBar.topItem?.title = ""
+    }
+}
+
+// MARK: - extensions
+extension CitySearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        viewModel.input.searchBarSearchButtonClicked.value = searchBar.text
+    }
+}
+
+extension CitySearchViewController: UITableViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        view.endEditing(true)
+        
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.height
+        
+        if offsetY > contentHeight - height {
+            if !viewModel.output.isLoadingLast.value {
+                viewModel.input.didScrollScrollView.value = ()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        viewModel.input.didSelectRowAt.value = indexPath
+    }
+}
+
+extension CitySearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.output.weathers.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CitySearchCell.identifier, for: indexPath) as? CitySearchCell else { return UITableViewCell() }
+        
+        cell.selectionStyle = .none
+        cell.bind(region: viewModel.output.regions.value[indexPath.row],
+                  weather: viewModel.output.weathers.value[indexPath.row],
+                  iconImage: viewModel.output.iconImages.value[indexPath.row])
+        
+        return cell
     }
 }
